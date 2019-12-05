9df1c420f16b0aa739d3695ae7670f776720f1c6
addonovan <austin@addonovan.com>
2019-12-04 21:17:45 -0600

    changes

--- cse4360/Hwk3/Q_learn.c
+++ cse4360/Hwk3/Q_learn.c
@@ -21,9 +21,28 @@ extern int old_state;                       /* Previous state           */
 /*                     0 if always the action with the highest          */
 /*                     Q value should be taken                          */
 /* Return value:  action number ( 0 or 1 )                              */
-int Q_learn(reinf, rand_act)
-double reinf;
-int rand_act;
-{
+int Q_learn(double reinf, int rand_act) {
+    // find the best action based on our current state
+    // argmax(Q_values[x][state])
+    int best_action = 0;
+    for (int i = 1; i < ACTIONS; i++) {
+        if (Q_values[i][state] > Q_values[best_action][state]) {
+            best_action = i;
+        }
+    }
 
+    // update the Q value
+    double old_q = Q_values[action][old_state];
+    double future_utility = Q_values[best_action][state];
+    double new_q = ((1 - BETA) * old_q) 
+                 + (BETA * (reinf + GAMMA * future_utility));
+    Q_values[action][old_state] = new_q;
+
+    // now, return the action we're supposed to
+    if (rand_act == 1) {
+        return bolzman_exploration(Q_values);
+    } else {
+        return best_action;
+    }
 }
+
